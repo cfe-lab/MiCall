905e20ec36bbb4592128e6c7857763035578e2eb
Eric Martin <emartin@cfenet.ubc.ca>
2013-06-18 16:24:12 -0700

    Shell script which tests different q cutoff conditions

---
+++ different_q_cutoffs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Use absolute paths
+# If referencing folders, do not put a slash at the end of the path
+# This shell script must be in the same directory as the pipeline
+
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_10_masked 10
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_15_masked 15
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_20_masked 20
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_25_masked 25
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_30_masked 30
+date
+python /Users/emartin/Desktop/Art_MiSeq_Pipeline_Git/miseqpipeline/pipeline3.py /Users/emartin/Desktop/130524_M01841_0004_000000000-A43J1/Data/Intensities/BaseCalls_35_masked 35
+date
