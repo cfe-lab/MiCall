c94f6cdf2681602d2507d72181fcd082ba2f3228
Art Poon <artpoon@gmail.com>
2013-07-23 15:55:53 -0700

    modified scripts 2 and 3 to execute on single file

--- 2_sam2fasta_with_base_censoring.py
+++ 2_sam2fasta_with_base_censoring.py
@@ -5,28 +5,22 @@ Output: <sample>.<region>.remap.sam.<qScore>.fasta
 
 import sys
 import os
-from glob import glob
+
 from sam2fasta import *
 
-if len(sys.argv) != 3:
-	print 'Usage: python 2_sam2fasta_with_censoring.py 130621_M01841_0006_000000000-A3RWC/Data/Intensities/BaseCalls/remap_sams/ 20'
+
+samfile = sys.argv[1]
+qCutoff = int(sys.argv[2])
+
+
+infile = open(samfile, 'rU')
+fasta = sam2fasta(infile, qCutoff)
+infile.close()
+
+if fasta is None:
 	sys.exit()
 
-remapPath = sys.argv[1]
-qCutoff = int(sys.argv[2])
-globPath = remapPath + '*.remap.sam'
-
-print "Globbing: {} ...\n".format(globPath)
-files = glob(globPath)
-
-for f in files:
-	print "sam2fasta({},{})".format(os.path.basename(f), qCutoff)
-	infile = open(f, 'rU')
-	fasta = sam2fasta(infile, qCutoff)
-	infile.close()
-	
-	if fasta:
-		outfilename = "{}.{}.fasta".format(f, qCutoff)
-		outfile = open(outfilename, 'w')
-		outfile.write(fasta)
-		outfile.close()
+outfilename = "{}.{}.fasta".format(samfile, qCutoff)
+outfile = open(outfilename, 'w')
+outfile.write(fasta)
+outfile.close()
--- 3_compress_fasta_with_min_count.py
+++ 3_compress_fasta_with_min_count.py
@@ -12,53 +12,37 @@ Output: <sample>.<region>.remap.sam.<qScore>.fasta.<minCount>.seq
 import sys
 from seqUtils import convert_fasta
 
-helpOutput = """
-Usage: python 3_compress_fasta_with_min_count.py <minCount> <fasta_files_to_collapse>
-Example (min count 3): python 3_compress_fasta_with_min_count.py 3 ../path/to/files/*.5.fasta
-"""
 
-if len(sys.argv) <= 2:
-	print helpOutput
-	sys.exit()
+f = sys.argv[1]
+minCount = int(sys.argv[2])
 
-minCount = int(sys.argv[1])
-files = sys.argv[2:]
+filename = f.split('/')[-1]
+prefix = filename.split('.')[0]
 
-if len(files) == 0:
-	print helpOutput
-	sys.exit()
 
-for f in files:
-	filename = f.split('/')[-1]
-	prefix = filename.split('.')[0]
-	
-	infile = open(f, 'rU')
-	try:
-		print "convert_fasta({})".format(filename)
-		fasta = convert_fasta(infile.readlines())
-	except:
-		print 'failed to convert', f
-		continue
-	
-	infile.close()
-	
-	# Collect identical sequences
-	d = {}
-	for h, s in fasta:
-		if d.has_key(s):
-			d[s] += 1
-		else:
-			d.update({s: 1})
-	
-	intermed = [(count, s) for s, count in d.iteritems()]
-	intermed.sort(reverse=True)
+infile = open(f, 'rU')
+fasta = convert_fasta(infile.readlines())
+infile.close()
 
-	outfilename = "{}.{}.seq".format(f,minCount)
-	#outfile = open(f+'.seq', 'w')
-	outfile = open(outfilename, 'w')
 
-	for i, (count, seq) in enumerate(intermed):
-		if count >= minCount:
-			outfile.write('>%s_variant_%d_count_%d\n%s\n' % (prefix, i, count, seq))
-	
-	outfile.close()
+# Collect identical sequences
+d = {}
+for h, s in fasta:
+	if d.has_key(s):
+		d[s] += 1
+	else:
+		d.update({s: 1})
+
+
+# sort sequences by count
+intermed = [(count, s) for s, count in d.iteritems()]
+intermed.sort(reverse=True)
+
+outfilename = "{}.{}.seq".format(f,minCount)
+outfile = open(outfilename, 'w')
+
+for i, (count, seq) in enumerate(intermed):
+	if count >= minCount:
+		outfile.write('>%s_variant_%d_count_%d\n%s\n' % (prefix, i, count, seq))
+
+outfile.close()
