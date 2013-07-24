29241943dfa18fa0fbb48faf3c7b451edfa16766
Art Poon <artpoon@gmail.com>
2013-07-24 15:02:07 -0700

    merged FASTA compression step into script 2, removed min count truncation, repaired g2p scoring script at 3

--- 2_sam2fasta_with_base_censoring.py
+++ 2_sam2fasta_with_base_censoring.py
@@ -6,22 +6,48 @@ Output: <sample>.<region>.remap.sam.<qScore>.fasta
 import sys
 import os
 
+from StringIO import StringIO
 from sam2fasta import *
-
+from seqUtils import *
 
 samfile = sys.argv[1]
 qCutoff = int(sys.argv[2])
 
+filename = samfile.split('/')[-1]
+prefix, region = filename.split('.')[:2]
 
+
+# convert SAM to FASTA by parsing CIGAR strings, censoring bases and merging reads
 infile = open(samfile, 'rU')
-fasta = sam2fasta(infile, qCutoff)
+fasta_str = sam2fasta(infile, qCutoff)
 infile.close()
 
-if fasta is None:
+if fasta_str is None:
 	sys.exit()
 
-# for now, do not reject sequences based on the proportion that are 'N'
-outfilename = "{}.{}.fasta".format(samfile, qCutoff, max_prop_N=1.0)
-outfile = open(outfilename, 'w')
-outfile.write(fasta)
+handle = StringIO(fasta_str)
+fasta = convert_fasta(handle.readlines())
+
+
+# collate identical sequences
+d = {}
+for h, s in fasta:
+	if d.has_key(s):
+		d[s] += 1
+	else:
+		d.update({s: 1})
+
+
+# sort sequences by count
+intermed = [(count, s) for s, count in d.iteritems()]
+intermed.sort(reverse=True)
+
+ofname = '.'.join(map(str, [samfile, qCutoff, 'fasta']))
+
+outfile = open(ofname, 'w')
+
+for i, (count, seq) in enumerate(intermed):
+	outfile.write('>%s_variant_%d_count_%d\n%s\n' % (prefix, i, count, seq))
+
 outfile.close()
+
