c3922689737dec4e22e96d4c51de099c2ba86519
Art Poon <artpoon@gmail.com>
2013-08-06 13:14:45 -0700

    more bugfixes

--- 0_MPI_wrapper.py
+++ 0_MPI_wrapper.py
@@ -7,11 +7,12 @@ import os
 from glob import glob
 from mpi4py import MPI
 from seqUtils import convert_fasta
+from datetime import datetime
 
 
-def prop_x4 (handle, cutoff, mincount):
+
+def prop_x4 (handle, cutoff, min_count):
 	fasta = convert_fasta(handle.readlines())
-
 	total_count = 0
 	total_x4_count = 0
 
@@ -26,8 +27,8 @@ def prop_x4 (handle, cutoff, mincount):
 			print 'ERROR, missing variant, count or fpr in header'
 			print h
 			sys.exit()
-	
-		if count <= minCount:
+		
+		if count <= min_count:
 			continue
 		
 		total_count += count
@@ -41,6 +42,14 @@ def prop_x4 (handle, cutoff, mincount):
 my_rank = MPI.COMM_WORLD.Get_rank()
 nprocs = MPI.COMM_WORLD.Get_size()
 
+
+def timestamp(msg):
+	print '[%s] (%d/%d) %s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+			my_rank,
+			nprocs,
+			msg)
+
+
 refpath = '/usr/local/share/miseq/refs/cfe'
 
 # iterative mapping of FASTQs to references
@@ -52,10 +61,13 @@ for i in range(len(files)):
 		continue
 	
 	filename = files[i].split('/')[-1]
-	print '... process %d of %d starting task 1_mapping.py on %s' % (my_rank, nprocs, filename)
+	#print '... process %d of %d starting task 1_mapping.py on %s' % (my_rank, nprocs, filename)
+	timestamp('1_mapping %s' % filename)
 	os.system('python 1_mapping.py %s %s' % (refpath, files[i]))
 
 MPI.COMM_WORLD.Barrier()
+
+
 
 # collect all remapped SAM files
 files = glob(root + '/*.remap.sam')
@@ -64,9 +76,11 @@ for i in range(len(files)):
 	if i % nprocs != my_rank:
 		continue
 	filename = files[i].split('/')[-1]
-	print '... process %d of %d starting task 2_sam2fasta on %s' % (my_rank, nprocs, filename)
+	#print '... process %d of %d starting task 2_sam2fasta on %s' % (my_rank, nprocs, filename)
+	timestamp('2_sam2fasta %s' % filename)
 	for qcut in [0, 10, 15, 20]:
 		os.system('python 2_sam2fasta_with_base_censoring.py %s %d' % (files[i], qcut))
+
 
 
 # compute g2p scores for env-mapped FASTAs
@@ -76,7 +90,8 @@ for i, file in enumerate(files):
 	if i % nprocs != my_rank:
 		continue
 	filename = files[i].split('/')[-1]
-        print '... process %d of %d starting task 3_g2p_scoring on %s' % (my_rank, nprocs, filename)
+	#print '... process %d of %d starting task 3_g2p_scoring on %s' % (my_rank, nprocs, filename)
+	timestamp('3_g2p_scoring %s' % filename)
 	os.system('python 3_g2p_scoring.py %s' % file) # generates *.v3prot and *.badV3
 
 
@@ -87,7 +102,8 @@ for i, file in enumerate(files):
 	if i % nprocs != my_rank:
 		continue
 	filename = files[i].split('/')[-1]
-	print '... process %d of %d starting task 5_amino_freqs on %s' % (my_rank, nprocs, filename)
+	#print '... process %d of %d starting task 5_amino_freqs on %s' % (my_rank, nprocs, filename)
+	timestamp('5_amino_freqs %s' % filename)
 	os.system('python 5_amino_freqs.py %s' % file)
 
 
