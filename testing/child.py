f200f4715ede8da39597776437f0ce39a237022b
Eric Martin <emartin@cfenet.ubc.ca>
2014-01-02 13:53:33 -0800

    Removed the leftover files from the previous version of the pipeline - still version 4.6

--- testing/child.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.stdout.write("this is standard out")
-#sys.stderr.write("this is standard error")
--- testing/parent.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-p = subprocess.Popen('python child.py', shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-stdout, stderr = p.communicate()
-
-print stdout == ""
-print stderr == ""
-
-#print stdout
-#print stderr
