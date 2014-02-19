394283a98ca85e91cdf833858a6b598b0cfd619e
Eric Martin <emartin@cfenet.ubc.ca>
2014-02-18 16:29:13 -0800

    Version 5 is our new production

--- minG2P.py
+++ minG2P.py
@@ -6,11 +6,11 @@ import sys, os, math
 import subprocess
 
 def conan_g2p (aaseq):
-	p = subprocess.Popen(['/usr/bin/ruby', 'g2p.rb', aaseq], stdout=subprocess.PIPE)
-	stdout, stderr = p.communicate()
-	try:
-		g2p, fpr, aligned = stdout.split('\n')[:3]
-	except ValueError:
-		return (None, None, None)
-	
-	return (g2p, fpr, aligned)
+    p = subprocess.Popen(['/usr/bin/ruby', 'g2p.rb', aaseq], stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    try:
+        g2p, fpr, aligned = stdout.split('\n')[:3]
+    except ValueError:
+        return (None, None, None)
+
+    return (g2p, fpr, aligned)
