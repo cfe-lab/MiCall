b64b675a61a7912bcb29823c55895ce1f32d59b6
don <donkirkby@gmail.com>
2014-08-06 14:17:20 -0700

    Check if Ruby Version Manager exists before configuring it.

--- fasta_to_g2p.sh
+++ fasta_to_g2p.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
 # The #! only allows one argument, so I couldn't get the -rubygems to work.
-source $HOME/.rvm/scripts/rvm
+if [ -f $HOME/.rvm/scripts/rvm ]; then
+    source $HOME/.rvm/scripts/rvm
+fi
 ruby -rubygems fasta_to_g2p.rb $*
