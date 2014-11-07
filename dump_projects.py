c05756a3624173ce4a1769b7f50b2c697865616c
don <donkirkby@gmail.com>
2014-11-07 09:37:00 -0800

    Warn of duplicate sets of key positions for issue #124.

--- dump_projects.py
+++ dump_projects.py
@@ -1,7 +1,32 @@
 import json
 import requests
+import sys
 
 import settings
+
+def check_key_positions(projects, warning_file):
+    """ Complain if a coordinate region has two sets of key positions in the
+    same project.
+    
+    @param projects: a dictionary of project definitions
+    @param warning_file: an open file to write any warnings in
+    """
+    warnings = set() # set([(project, region)])
+    for project_name, project in projects.iteritems():
+        regions_with_key_positions = set()
+        for region in project['regions']:
+            if region['key_positions']:
+                coordinate_name = region['coordinate_region']
+                if coordinate_name in regions_with_key_positions:
+                    warnings.add((project_name, coordinate_name))
+                regions_with_key_positions.add(coordinate_name)
+    for project_name, coordinate_name in sorted(warnings):
+        warning_file.write(
+            ("WARNING: project {} has multiple sets of key " +
+            "positions for coordinate region {}.\n").format(
+                project_name,
+                coordinate_name))
+    
 
 def main():
     dump = {}
@@ -17,11 +42,11 @@ def main():
                               auth=(settings.qai_user,
                                     settings.qai_password))
         projects = session.get(settings.qai_path + "/lab_miseq_projects.json?mode=dump",
-                              auth=(settings.qai_user,
-                                    settings.qai_password))
+                               auth=(settings.qai_user,
+                                     settings.qai_password))
         dump['regions'] = regions.json()
         dump['projects'] = projects.json()
-        
+        check_key_positions(dump['projects'], sys.stdout)
         
     with open("projects.json", "w") as f:
         json.dump(dump, f, sort_keys=True, indent=2, separators=(',', ': '))
---
+++ dump_projects_test.py
@@ -0,0 +1,152 @@
+import json
+import StringIO
+import unittest
+
+import dump_projects
+
+class CheckKeyPositionsTest(unittest.TestCase):
+    def setUp(self):
+        self.warningIO = StringIO.StringIO()
+        
+    def testSingleRegion(self):
+        projects = json.loads("""\
+{
+    "R1": {
+      "max_variants": 5,
+      "regions": [
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": null,
+                  "start_pos": 42
+              }
+          ],
+          "seed_region": "R1-seed"
+        }
+      ]
+    }
+}
+""")
+        expected_warnings = ""
+        
+        dump_projects.check_key_positions(projects, self.warningIO)
+        
+        self.assertMultiLineEqual(expected_warnings, self.warningIO.getvalue())
+        
+    def testMultipleRegionSingleSetOfPositions(self):
+        projects = json.loads("""\
+{
+    "R1": {
+      "max_variants": 5,
+      "regions": [
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": null,
+                  "start_pos": 42
+              }
+          ],
+          "seed_region": "R1a-seed"
+        },
+        {
+          "coordinate_region": "R1",
+          "key_positions": [],
+          "seed_region": "R1b-seed"
+        }
+      ]
+    }
+}
+""")
+        expected_warnings = ""
+        
+        dump_projects.check_key_positions(projects, self.warningIO)
+        
+        self.assertMultiLineEqual(expected_warnings, self.warningIO.getvalue())
+        
+    def testMultipleRegionTwoSetsOfPositions(self):
+        projects = json.loads("""\
+{
+    "R1": {
+      "max_variants": 5,
+      "regions": [
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": null,
+                  "start_pos": 42
+              }
+          ],
+          "seed_region": "R1a-seed"
+        },
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": 99,
+                  "start_pos": 1
+              }
+          ],
+          "seed_region": "R1b-seed"
+        }
+      ]
+    }
+}
+""")
+        expected_warnings = (
+            "WARNING: project R1 has multiple sets of key positions for " +
+            "coordinate region R1.\n")
+        
+        dump_projects.check_key_positions(projects, self.warningIO)
+        
+        self.assertMultiLineEqual(expected_warnings, self.warningIO.getvalue())
+        
+    def testMultipleRegionThreeSetsOfPositions(self):
+        projects = json.loads("""\
+{
+    "R1": {
+      "max_variants": 5,
+      "regions": [
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": null,
+                  "start_pos": 42
+              }
+          ],
+          "seed_region": "R1a-seed"
+        },
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": 99,
+                  "start_pos": 1
+              }
+          ],
+          "seed_region": "R1b-seed"
+        },
+        {
+          "coordinate_region": "R1",
+          "key_positions": [
+              {
+                  "end_pos": 14,
+                  "start_pos": 3
+              }
+          ],
+          "seed_region": "R1c-seed"
+        }
+      ]
+    }
+}
+""")
+        expected_warnings = (
+            "WARNING: project R1 has multiple sets of key positions for " +
+            "coordinate region R1.\n")
+        
+        dump_projects.check_key_positions(projects, self.warningIO)
+        
+        self.assertMultiLineEqual(expected_warnings, self.warningIO.getvalue())
