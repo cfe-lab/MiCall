029ec05d84ee57b76b263e4f52266621ea7eb42c
don <donkirkby@gmail.com>
2015-01-21 16:23:35 -0800

    Increment pipeline version.

--- settings_default.py
+++ settings_default.py
@@ -2,7 +2,7 @@
 To make pipeline portable, allow user to specify local paths and thread counts.
 """
 
-pipeline_version = '6.5'
+pipeline_version = '6.6'
 
 production = False  # set this to True to push results to NAS
 filter_cross_contaminants = False
