460216b5c8978422e98731a8a20662354e3dd956
don <donkirkby@gmail.com>
2015-11-02 10:38:46 -0800

    Updated projects.json, close #256.
    Also fixed #262 by checking for duplicate definitions.

--- micall/monitor/projects_dump.py
+++ micall/monitor/projects_dump.py
@@ -3,30 +3,41 @@ import sys
 
 from micall.monitor import qai_helper
 from micall import settings
+from collections import Counter
+
 
 def check_key_positions(projects, warning_file):
     """ Complain if a coordinate region has two sets of key positions in the
     same project.
-    
+
     @param projects: a dictionary of project definitions
     @param warning_file: an open file to write any warnings in
     """
-    warnings = set() # set([(project, region)])
+    key_warning = ("WARNING: project {} has multiple sets of key positions for "
+                   "coordinate region {}.\n")
+    seed_warning = "WARNING: project {} has duplicate seed and coordinate: {}, {}\n"
+    warnings = []
     for project_name, project in projects.iteritems():
         regions_with_key_positions = set()
+        seed_coordinate_pairs = Counter()
         for region in project['regions']:
+            coordinate_name = region['coordinate_region']
             if region['key_positions']:
-                coordinate_name = region['coordinate_region']
                 if coordinate_name in regions_with_key_positions:
-                    warnings.add((project_name, coordinate_name))
+                    warnings.append(key_warning.format(project_name,
+                                                       coordinate_name))
                 regions_with_key_positions.add(coordinate_name)
-    for project_name, coordinate_name in sorted(warnings):
-        warning_file.write(
-            ("WARNING: project {} has multiple sets of key " +
-            "positions for coordinate region {}.\n").format(
-                project_name,
-                coordinate_name))
-    
+            for seed_name in region['seed_region_names']:
+                seed_coordinate_pairs[(seed_name, coordinate_name)] += 1
+        sorted_counts = seed_coordinate_pairs.most_common()
+        for (seed_name, coordinate_name), count in sorted_counts:
+            if count > 1:
+                warnings.append(seed_warning.format(seed_name, coordinate_name))
+            else:
+                break
+    for warning in sorted(warnings):
+        warning_file.write(warning)
+
 
 def main():
     dump = {}
@@ -47,11 +58,11 @@ def main():
         if errors:
             raise StandardError('\n'.join(errors))
         check_key_positions(dump['projects'], sys.stdout)
-        
+
     with open("../projects.json", "w") as f:
         json.dump(dump, f, sort_keys=True, indent=2, separators=(',', ': '))
         f.write('\n')
-    
+
     print "Done."
 
 if __name__ == "__main__":
