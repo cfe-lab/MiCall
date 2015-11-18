360af7212049dc0c33b7ac233bc7f94b1142737b
don <donkirkby@gmail.com>
2015-11-18 15:28:02 -0800

    Compare average coverage instead of unmapped counts for #264.

--- micall/utils/remap_fastq_simplify.py
+++ micall/utils/remap_fastq_simplify.py
@@ -1,5 +1,5 @@
 import argparse
-from collections import defaultdict
+from collections import defaultdict, Counter
 import glob
 import logging
 import os
@@ -8,6 +8,9 @@ import subprocess
 from micall.core.miseq_logging import init_logging_console_only
 from micall.core.remap import remap
 from micall.core.prelim_map import prelim_map
+from csv import DictReader
+from micall.core.sam2aln import sam2aln
+from micall.core.aln2counts import aln2counts
 
 
 def write_simple_fastq(filename1, reads):
@@ -21,73 +24,107 @@ def write_simple_fastq(filename1, reads):
 
 
 def test(reads, simple_filename):
-    """ Map with 6.8 and 7.0, then compare unmapped read counts.
+    """ Map with 6.8 and 7.0, then compare average coverage in NS3.
 
-    @return: 'PASS' if the results match or it's a difference we're not
-        interested in, 'FAIL' otherwise
+    @return: 'PASS' if 7.0 coverage is as good as or better than 6.8 coverage,
+        'FAIL' otherwise
     """
     workdir = os.path.dirname(simple_filename)
     for bamfile in glob.glob(os.path.join(workdir, 'temp.bam*')):
         os.remove(bamfile)
 
     write_simple_fastq(simple_filename, reads)
-    unmapped_count68 = remap68(simple_filename)
-    unmapped_count70 = remap70(simple_filename)
-    if unmapped_count68 == unmapped_count70:
+    ns3_coverage68 = remap68(simple_filename)
+    ns3_coverage70 = remap70(simple_filename)
+    if ns3_coverage70 >= ns3_coverage68:
         return 'PASS'
-    print '6.8: {}, 7.0: {}'.format(unmapped_count68, unmapped_count70)
+    print '6.8: {}, 7.0: {}'.format(ns3_coverage68, ns3_coverage70)
     return 'FAIL'
 
 
 def remap68(fastq1_filename):
     workdir = os.path.dirname(fastq1_filename)
+    rundir = '/mnt/data/don/git/MiCall6_8'
     fastq2_filename = get_reverse_filename(fastq1_filename)
     prelim_filename = os.path.join(workdir, 'temp68.prelim.csv')
-    remap_filename = os.path.join(workdir, 'temp.remap.csv')
-    remap_counts_filename = os.path.join(workdir, 'temp.remap_counts.csv')
-    remap_conseq_filename = os.path.join(workdir, 'temp.remap_conseq.csv')
-    unmapped1_filename = os.path.join(workdir, 'temp.unmapped1.fastq')
-    unmapped2_filename = os.path.join(workdir, 'temp.unmapped2.fastq')
-    subprocess.check_call(['/mnt/data/don/git/MiCall6_8/prelim_map.py',
+    remap_filename = os.path.join(workdir, 'temp68.remap.csv')
+    remap_counts_filename = os.path.join(workdir, 'temp68.remap_counts.csv')
+    aligned_filename = os.path.join(workdir, 'temp68.aligned.csv')
+    nuc_filename = os.path.join(workdir, 'temp68.nuc.csv')
+    subprocess.check_call([os.path.join(rundir, 'prelim_map.py'),
                            fastq1_filename,
                            fastq2_filename,
                            prelim_filename],
-                          cwd='/mnt/data/don/git/MiCall6_8')
-    subprocess.check_call(['/mnt/data/don/git/MiCall6_8/remap.py',
+                          cwd=rundir)
+    subprocess.check_call([os.path.join(rundir, 'remap.py'),
                            fastq1_filename,
                            fastq2_filename,
                            prelim_filename,
                            remap_filename,
                            remap_counts_filename,
-                           remap_conseq_filename,
-                           unmapped1_filename,
-                           unmapped2_filename],
-                          cwd='/mnt/data/don/git/MiCall6_8')
-    return get_unmapped_counts(remap_counts_filename)
+                           os.devnull,
+                           os.devnull,
+                           os.devnull],
+                          cwd=rundir)
+    subprocess.check_call([os.path.join(rundir, 'sam2aln.py'),
+                           remap_filename,
+                           aligned_filename,
+                           os.devnull,
+                           os.devnull],
+                          cwd=rundir)
+    subprocess.check_call([os.path.join(rundir, 'aln2counts.py'),
+                           aligned_filename,
+                           nuc_filename,
+                           os.devnull,
+                           os.devnull,
+                           os.devnull,
+                           os.devnull,
+                           os.devnull],
+                          cwd=rundir)
+    return get_gene_coverage(nuc_filename, 'NS3')
 
 
-def get_unmapped_counts(remap_counts_filename):
-    with open(remap_counts_filename, 'rU') as remap_counts_csv:
-        unmapped_counts_line = list(remap_counts_csv)[-1]
-        unmapped_counts = int(unmapped_counts_line.split(',')[-2])
-    return unmapped_counts
+def get_gene_coverage(nuc_filename, gene_name):
+    gene_suffix = '-' + gene_name
+    counts = Counter()  # {pos: count}
+    with open(nuc_filename, 'rU') as nuc_csv:
+        reader = DictReader(nuc_csv)
+        for row in reader:
+            if row['region'].endswith(gene_suffix):
+                nuc_total = sum(map(int, (row[nuc] for nuc in 'ACGT')))
+                nuc_pos = int(row['refseq.nuc.pos'])
+                counts[nuc_pos] += nuc_total
+    if not counts:
+        return 0
+    return sum(counts.itervalues()) / float(len(counts))
+
+
+class DevNullWrapper(object):
+    def __init__(self, devnull):
+        self.devnull = devnull
+
+    def __getattr__(self, name):
+        return getattr(self.devnull, name)
+
+    def truncate(self):
+        pass
 
 
 def remap70(fastq1_filename):
     workdir = os.path.dirname(fastq1_filename)
     fastq2_filename = get_reverse_filename(fastq1_filename)
-    prelim_filename = os.path.join(workdir, 'temp.prelim.csv')
-    remap_filename = os.path.join(workdir, 'temp.remap.csv')
-    remap_counts_filename = os.path.join(workdir, 'temp.remap_counts.csv')
-    remap_conseq_filename = os.path.join(workdir, 'temp.remap_conseq.csv')
-    unmapped1_filename = os.path.join(workdir, 'temp.unmapped1.fastq')
-    unmapped2_filename = os.path.join(workdir, 'temp.unmapped2.fastq')
+    prelim_filename = os.path.join(workdir, 'temp70.prelim.csv')
+    remap_filename = os.path.join(workdir, 'temp70.remap.csv')
+    remap_counts_filename = os.path.join(workdir, 'temp70.remap_counts.csv')
+    aligned_filename = os.path.join(workdir, 'temp70.aligned.csv')
+    nuc_filename = os.path.join(workdir, 'temp70.nuc.csv')
     with open(prelim_filename, 'w+') as prelim_csv, \
             open(remap_filename, 'w+') as remap_csv, \
             open(remap_counts_filename, 'w+') as remap_counts_csv, \
-            open(remap_conseq_filename, 'w+') as remap_conseq_csv, \
-            open(unmapped1_filename, 'w+') as unmapped1, \
-            open(unmapped2_filename, 'w+') as unmapped2:
+            open(aligned_filename, 'w+') as aligned_csv, \
+            open(nuc_filename, 'w+') as nuc_csv, \
+            open(os.devnull, 'w+') as real_devnull:
+        devnull = DevNullWrapper(real_devnull)
         prelim_map(fastq1_filename, fastq2_filename, prelim_csv)
         prelim_csv.seek(0)
         remap(fastq1_filename,
@@ -95,10 +132,20 @@ def remap70(fastq1_filename):
               prelim_csv,
               remap_csv,
               remap_counts_csv,
-              remap_conseq_csv,
-              unmapped1,
-              unmapped2)
-    return get_unmapped_counts(remap_counts_filename)
+              devnull,
+              devnull,
+              devnull)
+        remap_csv.seek(0)
+        sam2aln(remap_csv, aligned_csv, devnull, devnull)
+        aligned_csv.seek(0)
+        aln2counts(aligned_csv,
+                   nuc_csv,
+                   devnull,
+                   devnull,
+                   devnull,
+                   devnull,
+                   devnull)
+    return get_gene_coverage(nuc_filename, 'NS3')
 
 
 def ddmin(reads, simple_filename):
@@ -156,11 +203,13 @@ def read_fastq(filename, reads):
 
 
 def compare_remap(txtfilename, logger):
-    unmapped_count68 = remap68(txtfilename)
-    unmapped_count70 = remap70(txtfilename)
+    ns3_coverage68 = remap68(txtfilename)
+    ns3_coverage70 = remap70(txtfilename)
 
-    if unmapped_count68 != unmapped_count70:
-        print 'Mismatch with sample', txtfilename
+    if ns3_coverage70 < ns3_coverage68:
+        print 'Mismatch with sample {}: {} -> {}'.format(txtfilename,
+                                                         ns3_coverage68,
+                                                         ns3_coverage70)
         reads = defaultdict(list)
         read_fastq(txtfilename, reads)
         read_count = len(reads)
@@ -186,7 +235,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     logger = init_logging_console_only(logging.INFO)
-    for txtfilename in glob.glob(os.path.join(args.workdir, args.pattern)):
+    filenames = glob.glob(os.path.join(args.workdir, args.pattern))
+    filenames.sort()
+    for txtfilename in filenames:
         if txtfilename.find('simple') < 0:
             logger.info(os.path.basename(txtfilename))
             compare_remap(txtfilename, logger)
