dae1a88d1ce9ebd88732ce255fa4a0289eb2a9ac
don <donkirkby@gmail.com>
2016-06-28 14:35:27 -0700

    Fix some PEP8 warnings.

--- micall/utils/collate.py
+++ micall/utils/collate.py
@@ -1,12 +1,12 @@
 import glob
 import os
 
-#TODO: This can probably be deleted. Wait until we revisit cross-contamination filter to be sure.
+# TODO: This can probably be deleted. Wait until we revisit cross-contamination filter to be sure.
 # def collate_frequencies (run_path, output_path, output_type):
 #     """
 #     Collate amino/nuc .freq files into a single summary file.
 #     """
-# 
+#
 #     if output_type == "amino":
 #         file_extension = "amino.csv"
 #         header = "sample,region,q-cutoff,query.aa.pos,refseq.aa.pos,A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y,*"
@@ -15,22 +15,23 @@ import os
 #         header = "sample,region,q-cutoff,query.nuc.pos,refseq.nuc.pos,A,C,G,T"
 #     else:
 #         raise Exception("Incorrect output_type parameter")
-# 
+#
 #     # Summarize uncleaned freqs
 #     with open(output_path, "w") as f_out:
 #         f_out.write("{}\n".format(header))
-#         for path in [x for x in glob.glob("{}/*.{}".format(run_path,file_extension)) if "clean.{}".format(file_extension) not in x]:
+#         for path in [x for x in glob.glob("{}/*.{}".format(run_path, file_extension))
+#                      if "clean.{}".format(file_extension) not in x]:
 #             prefix = (os.path.basename(path)).rstrip(".{}".format(file_extension))
 #             sample = prefix.split(".")[0]
 #             with open(path,"r") as f:
 #                 lines = f.readlines()
-# 
+#
 #             for line in lines:
 #                 f_out.write("{},{}\n".format(sample, line.rstrip("\n")))
-# 
+#
 #     # Summarize clean freqs
 #     file_extension = "clean.{}".format(file_extension)
-# 
+#
 #     output_path = output_path.replace("_frequencies.csv", "_cleaned_frequencies.csv")
 #     with open(output_path, "w") as f_out:
 #         f_out.write("{}\n".format(header))
@@ -39,7 +40,7 @@ import os
 #             sample, region, q = prefix.split(".")[:3]
 #             with open(path,"r") as f:
 #                 lines = f.readlines()
-# 
+#
 #             for j, line in enumerate(lines):
 #                 if j == 0:
 #                     continue
@@ -72,9 +73,10 @@ def collate_named_files(src_dir, sample_list, extension, output_path):
                         fout.write(samplename + ',')
                         fout.write(line)
 
+
 def collate_labeled_files(pattern, output_path):
     """ Collate files that are already labeled on each row.
-    
+
     Just remove redundant headers and concatenate all the files together.
     @param pattern: search pattern for all the files to collate
     @param param: output_path the path to write the results to
