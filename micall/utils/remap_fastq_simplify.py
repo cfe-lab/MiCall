b9f2785627f1678023173b2212d96d3811f4e809
don <donkirkby@gmail.com>
2016-07-14 17:05:41 -0700

    Change remap simplify to search for gap in NS2.

--- micall/utils/remap_fastq_simplify.py
+++ micall/utils/remap_fastq_simplify.py
@@ -11,22 +11,55 @@ from micall.core.sam2aln import sam2aln
 from micall.core.aln2counts import aln2counts, AMINO_ALPHABET
 from micall.utils.dd import DD
 
+BOWTIE_THREADS = 11
+
 
 class MicallDD(DD):
     def __init__(self, filename1):
         super(MicallDD, self).__init__()
-        self.filename1 = filename1
+        self.filename1 = self.filter_fastqs(filename1)
         reads = defaultdict(list)
-        read_fastq(filename1, reads)
+        read_fastq(self.filename1, reads)
         read_count = len(reads)
-        read_fastq(get_reverse_filename(filename1), reads)
+        read_fastq(get_reverse_filename(self.filename1), reads)
         added_count = len(reads) - read_count
         if added_count > 0:
             raise RuntimeError('Found {} new reads.'.format(added_count))
         self.reads = reads.values()
 
+    def filter_fastqs(self, filename1):
+        filter_name1 = filename1 + '_filter.fastq'
+        if os.path.exists(filter_name1):
+            logging.info('Already filtered.')
+            return filter_name1
+        filename2 = get_reverse_filename(filename1)
+        filter_name2 = filename2 + '_filter.fastq'
+        workdir = os.path.dirname(filename1)
+        prelim_filename = os.path.join(workdir, 'temp70.prelim.csv')
+        with open(prelim_filename, 'w+') as prelim_csv:
+            prelim_map(filename1,
+                       filename2,
+                       prelim_csv,
+                       nthreads=BOWTIE_THREADS)
+            prelim_csv.seek(0)
+            reader = DictReader(prelim_csv)
+            mapped_qnames = {row['qname']
+                             for row in reader
+                             if row['rname'].startswith('HCV') and 2400 < int(row['pos']) < 3700}
+        self.filter_reads(filename1, filter_name1, mapped_qnames)
+        self.filter_reads(filename2, filter_name2, mapped_qnames)
+        logging.info('Finished filtering with %d reads.', len(mapped_qnames))
+        return filter_name1
+
+    def filter_reads(self, filename, filter_name, qnames):
+        with open(filename, 'rU') as fin, open(filter_name, 'w') as fout:
+            for read in zip(fin, fin, fin, fin):
+                qname = read[0].split()[0][1:]
+                if qname in qnames:
+                    for line in read:
+                        fout.write(line)
+
     def _test(self, read_indexes, debug_file_prefix=None):
-        BOWTIE_THREADS = 11
         simple_filename1 = self.filename1 + '_simple.fastq'
         self.write_simple_fastq(simple_filename1, read_indexes)
         workdir = os.path.dirname(self.filename1)
@@ -79,22 +112,63 @@ class MicallDD(DD):
                        devnull,
                        devnull)
 
+        return self.get_result(amino_filename, len(read_indexes))
+
+    def disabled_resolve(self, csub, c, direction):
+        sub_size = len(csub)
+        if direction == DD.REMOVE:
+            # result = csub[:sub_size/2]
+            result = None
+        else:
+            # ADD
+            add_count = (len(c) - sub_size + 1) / 2
+            result = []
+            for i in c:
+                if i in csub:
+                    result.append(i)
+                elif add_count > 0:
+                    result.append(i)
+                    add_count -= 1
+        return result
+
+    def get_result(self, amino_filename, read_count):
         with open(amino_filename, 'rU') as amino_csv:
             amino_reader = DictReader(amino_csv)
             region_counts = [row
                              for row in amino_reader
-                             if row['region'] == 'HCV3-S52-E2']
+                             if row['region'] == 'HCV1A-H77-NS2']
         region_coverage = [sum(int(row[aa]) for aa in AMINO_ALPHABET if aa != '*')
                            for row in region_counts]
         if not region_coverage:
+            logging.info('PASS: no region coverage, read_count=%d.', read_count)
             return DD.PASS
         max_coverage = max(region_coverage)
-        if max_coverage < 5 or region_coverage[1] < max_coverage * 0.7:
-            # print('{} max {}'.format(region_coverage[1], max_coverage))
+        if max_coverage < max(10, 0.1*read_count):
+            logging.info('PASS: max_coverage=%d, read_count=%d.',
+                         max_coverage,
+                         read_count)
             return DD.PASS
-        if region_coverage[0] == 0:
+        early_coverage = region_coverage[:76]
+        late_coverage = region_coverage[76:]
+        early_avg = sum(early_coverage)*1.0/len(early_coverage)
+        late_avg = sum(late_coverage)*1.0/len(late_coverage)
+        late_min = min(late_coverage)
+        if late_min < min(10, 0.5*max_coverage):
+            logging.info('PASS: late_min=%d, max_coverage=%d, read_count=%d',
+                         late_min,
+                         max_coverage,
+                         read_count)
+            return DD.PASS
+        if early_avg < 0.01*late_avg:
+            logging.info('FAIL: early_avg=%f, late_avg=%f, read_count=%d',
+                         early_avg,
+                         late_avg,
+                         read_count)
             return DD.FAIL
-        # print('{}, {} max {}'.format(region_coverage[0], region_coverage[1], max_coverage))
+        logging.info('PASS: early_avg=%f, late_avg=%f, read_count=%d',
+                     early_avg,
+                     late_avg,
+                     read_count)
         return DD.PASS
 
     def write_simple_fastq(self, filename1, read_indexes):
@@ -108,15 +182,17 @@ class MicallDD(DD):
                     f2.write(line)
 
     def coerce(self, c):
+        if c is None:
+            return 'None'
         blocks = []  # [[first, last]] indexes for all contiguous blocks
         for i in c:
             if (not blocks) or blocks[-1][-1] != i-1:
                 blocks.append([i, i])
             else:
                 blocks[-1][-1] = i
-        return ', '.join(str(block[0]) if block[0] == block[1]
-                         else '{}-{}'.format(*block)
-                         for block in blocks)
+        return '[' + ', '.join(str(block[0]) if block[0] == block[1]
+                               else '{}-{}'.format(*block)
+                               for block in blocks) + ']'
 
 
 class DevNullWrapper(object):
@@ -138,7 +214,7 @@ def read_fastq(filename, reads):
     """ Load all the reads from a FASTQ file into a list.
 
     @param filename: the FASTQ file to open
-    @param reads: defaultdict({line1: [line2, line3, line4, line2, line3, line4]}
+    @param reads: defaultdict({qname: [line1, line2, line3, line4, line1, line2, line3, line4]}
     """
     with open(filename, 'rU') as f:
         for line1, line2, line3, line4 in zip(f, f, f, f):
@@ -156,8 +232,12 @@ def main():
         logger.info('Starting.')
         fname = ('censored1.fastq')
         dd = MicallDD(fname)
-        read_indexes = range(len(dd.reads))
-        min_indexes = dd.ddmin(read_indexes[50:85])
+        read_indexes = range(len(dd.reads))[:500]
+        run_test = True
+        if run_test:
+            min_indexes = dd.ddmin(read_indexes)
+        else:
+            min_indexes = read_indexes
         dd._test(min_indexes, debug_file_prefix='micall_debug')
         # dd.write_simple_fastq(fname + '_min.fastq', min_indexes)
         logger.info('Done.')
