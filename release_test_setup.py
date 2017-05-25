7854a241d3f3a585dcb98ae92346bd4a87090ac8
don <donkirkby@gmail.com>
2017-05-24 17:04:27 -0700

    Set up test runs for all recent MiSeq batches as part of #381.

--- release_test_setup.py
+++ release_test_setup.py
@@ -17,16 +17,24 @@ from operator import attrgetter
 import os
 from shutil import copytree, copy, rmtree
 
-from micall.settings import rawdata_mount, pipeline_version, ERROR_PROCESSING
+from micall.settings import rawdata_mount, pipeline_version, ERROR_PROCESSING, \
+    NEEDS_PROCESSING
 
 
 def parse_args():
     parser = ArgumentParser(description='Prepare sample runs for testing a new release.')
-    parser.add_argument('source_folder',
-                        help='Main RAWDATA folder that will be copied to local RAWDATA.')
-    parser.add_argument('-s', '--samples_csv',
-                        default='test_samples.csv',
-                        help='CSV file with sample and run names.')
+    parser.add_argument(
+        'source_folder',
+        help='Main RAWDATA folder that will be copied to local RAWDATA.')
+    parser.add_argument(
+        '-s',
+        '--samples_csv',
+        default='test_samples.csv',
+        help='CSV file with sample and run names.')
+    parser.add_argument(
+        '-m',
+        '--min_run_name',
+        help='Select all later runs, (e.g., "161201"). Overrides samples_csv.')
     # parser.add_argument('-r', '--run_name',
     #                     help='Run name to set up. Overrides samples_csv.')
     # parser.add_argument('-e', '--enum',
@@ -41,19 +49,23 @@ class Sample(object):
         self.fastq_paths = None
 
     def find(self, source_folder):
-        run_parts = self.run_name.split('.')
-        if len(run_parts) < 2:
-            run_parts.append('M01841')
-        format_string = '%d-%b-%y' if len(run_parts[0]) == 9 else '%d-%b-%Y'
-        run_date = datetime.strptime(run_parts[0], format_string)
-        base_run_name = run_date.strftime('%y%m%d') + '_' + run_parts[1]
-        pattern = os.path.join(source_folder, 'MiSeq', 'runs', base_run_name+'*')
-        matches = glob(pattern)
-        if len(matches) != 1:
-            raise RuntimeError('Expected one match for {}, but found: {}'.format(
-                pattern,
-                matches))
-        self.run_name = matches[0]
+        run_path = os.path.join(source_folder, 'MiSeq', 'runs', self.run_name)
+        if os.path.exists(run_path):
+            self.run_name = run_path
+        else:
+            run_parts = self.run_name.split('.')
+            if len(run_parts) < 2:
+                run_parts.append('M01841')
+            format_string = '%d-%b-%y' if len(run_parts[0]) == 9 else '%d-%b-%Y'
+            run_date = datetime.strptime(run_parts[0], format_string)
+            base_run_name = run_date.strftime('%y%m%d') + '_' + run_parts[1]
+            pattern = os.path.join(source_folder, 'MiSeq', 'runs', base_run_name+'*')
+            matches = glob(pattern)
+            if len(matches) != 1:
+                raise RuntimeError('Expected one match for {}, but found: {}'.format(
+                    pattern,
+                    matches))
+            self.run_name = matches[0]
 
         pattern = os.path.join(self.run_name,
                                'Data',
@@ -65,6 +77,10 @@ class Sample(object):
             raise RuntimeError('No matches found for ' + pattern)
         matches.sort()
         self.fastq_paths = matches
+        run_parts = os.path.basename(self.run_name).split('_')
+        run_date = datetime.strptime(run_parts[0], '%y%m%d')
+        qai_run_name = run_date.strftime('%d-%b-%Y') + '.' + str(run_parts[1])
+        print('LabMiseqRun.import("{}")'.format(qai_run_name))
 
     def setup_samples(self):
         base_run_name = os.path.basename(self.run_name)
@@ -90,7 +106,7 @@ class Sample(object):
                         os.rename(suspended_fastq_path,
                                   target_path)
                     else:
-                        copy(fastq_path, target_path)
+                        os.symlink(fastq_path, target_path)
         return sample_paths
 
     def setup_run(self):
@@ -111,8 +127,8 @@ class Sample(object):
                                            'Intensities',
                                            'BaseCalls')
             os.makedirs(base_calls_path)
-            copytree(os.path.join(self.run_name, 'InterOp'),
-                     os.path.join(target_run_path, 'InterOp'))
+            os.symlink(os.path.join(self.run_name, 'InterOp'),
+                       os.path.join(target_run_path, 'InterOp'))
             for filename in ('RunInfo.xml',
                              'runParameters.xml',
                              'SampleSheet.csv',
@@ -169,11 +185,32 @@ def suspend_inactive_samples(run_path, active_sample_paths):
             raise
 
 
+def find_run_folders(source_folder, min_run_name):
+    pattern = os.path.join(source_folder, 'MiSeq', 'runs', '*')
+    matches = glob(pattern)
+    folder_names = [os.path.basename(match) for match in matches]
+    recent_folders = [folder
+                      for folder in folder_names
+                      if folder >= min_run_name and folder[0].isnumeric()]
+    folders_with_data = [folder
+                         for folder in recent_folders
+                         if os.path.exists(os.path.join(source_folder,
+                                                        'MiSeq',
+                                                        'runs',
+                                                        folder,
+                                                        NEEDS_PROCESSING))]
+    return folders_with_data
+
+
 def main():
     args = parse_args()
-    with open(args.samples_csv, 'rU') as samples_csv:
-        samples = [Sample(row['run'], row['enum'])
-                   for row in DictReader(samples_csv)]
+    if args.min_run_name is not None:
+        run_folders = find_run_folders(args.source_folder, args.min_run_name)
+        samples = [Sample(folder, '*') for folder in run_folders]
+    else:
+        with open(args.samples_csv, 'rU') as samples_csv:
+            samples = [Sample(row['run'], row['enum'])
+                       for row in DictReader(samples_csv)]
     for sample in samples:
         sample.find(args.source_folder)
     samples.sort(key=lambda s: (s.run_name, s.extract_num))
