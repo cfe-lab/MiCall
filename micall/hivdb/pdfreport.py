b5d3fee88d96c327015440e778a21e325fa84493
Walter Scott <wscott@cfenet.ubc.ca>
2017-06-07 10:36:32 -0700

    - adjusted column widths in drug class tables (drug names take less space) - bold table header text

--- micall/hivdb/pdfreport.py
+++ micall/hivdb/pdfreport.py
@@ -11,6 +11,7 @@ from reportlab.lib.styles import ParagraphStyle
 
 import reportlab.platypus as plat
 
+# we currently only support North American letter paper -- no A4
 page_w, page_h = letter
 
 top_margin = 2 * cm
@@ -26,60 +27,60 @@ def bottom_para(txt):
 def headertab_style(colnum, dospan):
     """Generate a style list for the first row of a table with colnum columns.
     dospan := turn the colnum columns into a single one with centred text"""
-    lst = [
-        ("TEXTCOLOR", (0, 0), (colnum-1, 0), colors.white),
-        ("BACKGROUND", (0, 0), (colnum-1, 0), colors.green),
-        ("GRID", (0, 0), (colnum-1, 0), 1, colors.black),
-        ("ALIGN", (0, 0), (colnum-1, 0), "CENTRE")]
+    lst = [("TEXTCOLOR", (0, 0), (colnum-1, 0), colors.white),
+           ("BACKGROUND", (0, 0), (colnum-1, 0), colors.green),
+           ("ALIGN", (0, 0), (colnum-1, 0), "CENTRE"),
+           ("FACE", (0, 0), (colnum-1, 0), "Helvetica-Bold")]
     if dospan:
         lst.extend([("SPAN", (0, 0), (colnum-1, 0)),
                     ("BOX", (0, 0), (colnum-1, 0), 1, colors.black)])
+    else:
+        lst.extend([("GRID", (0, 0), (colnum-1, 0), 1, colors.black)])
     return lst
 
 
-def drug_class_table(cfg_dct, dc_name, level_coltab):
-    """Generate a resistance report for a given drug class
-    lr_align: str 'LEFT' or 'RIGHT': alignment of table on page.
+def drug_class_table(cfg_dct, dc_name, level_coltab, tabwidth):
+    """Generate a resistance report for a given drug class.
+    tabwidth: the total width allocated for the table.
     """
     drug_lst = cfg_dct["known_drugs"][dc_name]
     table_header_str = cfg_dct['drug_class_tableheaders'][dc_name]
     resistance_dct = cfg_dct["res_results"]
     mutation_str = cfg_dct["mutation_strings"][dc_name]
-    # header column: name of drug_class
-    num_drugs = len(drug_lst)
-    drow_min, drow_max = 1,  num_drugs
+    # 1) row 0: header column: name of drug_class
     t_data = [["{} Drugs".format(table_header_str), ""]]
-    t_style = [("GRID", (0, drow_min), (1, drow_max), 1, colors.black)]
-    # header box style
-    t_style.extend(headertab_style(2, dospan=True))
-    # list of drugs in this drug_class
+    t_style = headertab_style(2, dospan=True)
+    # 2) row 1..num_drugs: list of drugs in this drug_class
+    drow_min, drow_max = 1,  len(drug_lst)
+    t_style.extend([("GRID", (0, drow_min), (1, drow_max), 1, colors.black),
+                    ("ALIGNMENT", (0, drow_min), (0, drow_max), 'LEFT')])
     for tabline, dd in enumerate(drug_lst):
         drug_id, drug_name = dd
         if drug_id in resistance_dct:
             level, level_name = resistance_dct[drug_id]
         else:
             level, level_name = 1, "NOT REPORTED"
+        t_data.append([drug_name.capitalize(), level_name])
         # determine colours for the level
         bg_col, fg_col = level_coltab[level]
-        t_style.append(('TEXTCOLOR', (1, tabline + 1), (1, tabline + 1),
-                        fg_col))
-        t_style.append(('BACKGROUND', (1, tabline + 1), (1, tabline + 1),
-                        bg_col))
-        t_data.append([drug_name.capitalize(), level_name])
-    # mutation string
-    # a separate style list for the 'relevant mutations' box
+        t_style.extend([('TEXTCOLOR', (1, tabline + 1), (1, tabline + 1), fg_col),
+                        ('BACKGROUND', (1, tabline + 1), (1, tabline + 1), bg_col)])
+    # 3) mutation string
+    # we put this into a separate paragraph into a column that spans the two table columns
     mut_row = drow_max + 1
-    rel_mut_style = [("SPAN", (0, mut_row), (1, mut_row)),
-                     ("BOX", (0, mut_row), (1, mut_row), 1, colors.black)]
-    t_style.extend(rel_mut_style)
-    mut_para_style = ParagraphStyle("sconormal")
-    t_data.append([plat.Paragraph(mutation_str, mut_para_style), ""])
+    t_style.extend([("SPAN", (0, mut_row), (1, mut_row)),
+                    ("BOX", (0, mut_row), (1, mut_row), 1, colors.black)])
+    t_data.append([plat.Paragraph(mutation_str, ParagraphStyle("sconormal")), ""])
     assert sum([len(row) == 2 for row in t_data]) == len(t_data), "wonky drug table"
-    return plat.Table(t_data, vAlign="TOP", style=t_style)  # , hAlign=lr_align)
+    # NOTE: this fudge factor ensures that the left, drug_name column, is not too wide.
+    colw = tabwidth * 0.36
+    return plat.Table(t_data, vAlign="TOP", style=t_style, colWidths=[colw, None])
 
 
 def top_table(sample_name, colwidth):
-    """Generate a mostly empty top table."""
+    """Generate a mostly empty top table.
+    colwidth: the width of columns 0, and 2. The width of columns 1 is determined automatically.
+    """
     samp_name = sample_name or "None"
     # get the time, ignoring microseconds
     nowstr = str(datetime.datetime.utcnow().replace(microsecond=0))
@@ -88,18 +89,16 @@ def top_table(sample_name, colwidth):
                ["", "Report Date (UTC): {}".format(nowstr), ""],
                ["", "", ""]
                ]
-    T_lines = len(test_dl)
-    rn_min, rn_max = 1, T_lines - 1
+    rn_min, rn_max = 1, len(test_dl) - 1
     lc, mc, rc = 0, 1, 2
-    st_lst = [("BOX", (lc, rn_min), (lc, rn_max), 1, colors.black),
-              ("BOX", (rc, rn_min), (rc, rn_max), 1, colors.black),
-              ("GRID", (mc, rn_min), (mc, rn_max), 1, colors.black),
-              ("FONTSIZE", (0, rn_min), (2, rn_max), 8)]
-    # colwidth -= 1.0 * cm
-    st_lst.extend(headertab_style(3, dospan=False))
-    tt = plat.Table(test_dl, style=st_lst, colWidths=[colwidth, None, colwidth])
-    tt.hAlign = "CENTRE"
-    return tt
+    st_lst = headertab_style(3, dospan=False)
+    st_lst.extend([("BOX", (lc, rn_min), (lc, rn_max), 1, colors.black),
+                   ("BOX", (rc, rn_min), (rc, rn_max), 1, colors.black),
+                   ("GRID", (mc, rn_min), (mc, rn_max), 1, colors.black),
+                   ("FONTSIZE", (lc, rn_min), (rc, rn_max), 8)])
+    return plat.Table(test_dl, style=st_lst,
+                      colWidths=[colwidth, None, colwidth],
+                      hAlign="CENTRE")
 
 
 def write_report(cfg_dct, res_lst, mut_lst, fname, sample_name=None):
@@ -115,7 +114,7 @@ def write_report(cfg_dct, res_lst, mut_lst, fname, sample_name=None):
         author="BCCfE")
     # get the actual text width, (not the page width):
     txt_w = page_w - doc.leftMargin - doc.rightMargin
-    w_half, w_top = txt_w * 0.5, txt_w / 3.0
+    w_half, top_table_col_width = txt_w * 0.5, txt_w / 3.3333
     doc_els = [plat.Spacer(1, 1.5 * cm)]
     ti_style = ParagraphStyle("scotitle", alignment=TA_CENTER, fontSize=20)
     doc_els.append(plat.Paragraph(cfg_dct["report_title"], ti_style))
@@ -123,7 +122,7 @@ def write_report(cfg_dct, res_lst, mut_lst, fname, sample_name=None):
                               spaceBefore=5 * mm, spaceAfter=5 * mm)
     doc_els.append(plat.Paragraph("For research use only", re_style))
     # -- top table
-    doc_els.append(top_table(sample_name, w_top))
+    doc_els.append(top_table(sample_name, top_table_col_width))
     lc, rc = 0, 1
     big_table, btstyle = [], []
     # now drug classes tables, two per line
@@ -131,7 +130,7 @@ def write_report(cfg_dct, res_lst, mut_lst, fname, sample_name=None):
     # from the resistance, we determine which drug_classes to write a table for:
     # we only write a table if we are given resistance data for it.
     got_dc_set = set([dc["drug_class"] for dc in res_lst])
-    tl = [drug_class_table(cfg_dct, dc, level_coltab) for dc in known_dc_lst if dc in got_dc_set]
+    tl = [drug_class_table(cfg_dct, dc, level_coltab, w_half) for dc in known_dc_lst if dc in got_dc_set]
     d_rowmin = 0
     while len(tl) > 0:
         row_lst = [tl.pop(0)]
