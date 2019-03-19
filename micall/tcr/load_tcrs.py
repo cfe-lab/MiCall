e08618882bc7af9cdb821edb50e123a4d400ec3d
jeff-k <jeff@x-0.ca>
2019-03-19 13:39:08 -0700

    load TCR fastq files and bin unique fragments

---
+++ micall/tcr/load_tcrs.py
@@ -0,0 +1,70 @@
+import sys
+from tcr.igblast import igblast_seq, parse_fmt3
+
+def load_bins_strict(path):
+    fastq = []
+    for line in open(path):
+        fastq.append(line.strip()) 
+
+    tra, trb, runts = {}, {}, {}
+    for _, seq, _, _ in zip(*(iter(fastq),) * 4):
+        if len(seq) < 60:
+            if seq not in runts:
+                runts[seq] = 0
+            runts[seq] += 1
+            continue
+
+        if len(seq) < 200:
+            continue
+
+        if "CCCACCCGAGGTCGCTG" in seq:
+            if seq not in trb:
+                trb[seq] = 0
+            trb[seq] += 1
+
+        if "ACCAGCTGAGAGACTCT" in seq:
+            if seq not in tra:
+                tra[seq] = 0
+            tra[seq] += 1
+
+        if "CCCACCCGAGGTCGCTG" in seq and "ACCAGCTGAGAGACTCT" in seq:
+            print("WOW BOTH CONSTANTS APPEAR IN", seq)
+    
+    return tra, trb, runts
+
+def load_bins_whatever(path):
+    fastq = []
+    for line in open(path):
+        fastq.append(line.strip()) 
+
+    bins, runts = {} 
+    for _, seq, _, _ in zip(*(iter(fastq),) * 4):
+        if len(seq) < 60:
+            if seq not in runts:
+                runts[seq] = 0
+            runts[seq] += 1
+            continue
+
+        if len(seq) < 200:
+            continue
+
+        if seq not in bins:
+            bins[seq] = 0
+        bins[seq] += 1
+
+    return bins, runts
+
+def id_bins(bins):
+    groups = {}
+    for k in bins:
+        if bins[k] < 500:
+            continue
+        calls = parse_fmt3(igblast_seq(k))[0][1]
+        genes = []
+        for gene in calls: 
+            gt = ';'.join(map(lambda x: x[0], calls[gene]))
+            genes.append(gt)
+        if tuple(genes) not in groups:
+            groups[tuple(genes)] = 0
+        groups[tuple(genes)] += bins[k]
+    return groups
