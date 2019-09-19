0157dbf5c038609e3522d96ccf4bfe555e3a5f7f
ArtPoon <artpoon@gmail.com>
2019-09-19 15:49:06 -0400

    modified setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from distutils.core import setup, Extension
 import sys
 
@@ -6,8 +7,20 @@ if sys.version_info < (3,):
     print('Try Micall:\n  http://github.com/cfe-lab/MiCall')
     sys.exit()
 
+with open('README.md') as fh:
+    long_description = fh.read()
+
 setup(
-    name='micall', 
+    name='MiCall-Lite',
+    version='v0.1',
+    author=['Art Poon', 'Don Kirkby', 'Eric Martin', 'Richard H. Liang'], 
+    author_email='apoon42@uwo.ca',
+
+    description='Pipeline for processing FASTQ data from an Illumina MiSeq for human RNA virus (HIV, hepatitis C virus) genotyping',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PoonLab/MiCall-Lite",
+
     packages=['micall', 'micall.core', 'micall.utils', 'micall.alignment'],
     scripts=['bin/micall'],
     ext_modules=[Extension('micall.alignment._gotoh2',
