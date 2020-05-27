980bc2556e2b893d47c384c73cb4ab085948319b
dan.msi <dan.sendbot@gmail.com>
2020-05-27 12:05:41 -0700

    Added data_extended for summary results. Renamed data.tsv to data.csv. Modified some output info.

--- micall/utils/conseq_compare.py
+++ micall/utils/conseq_compare.py
@@ -1,8 +1,10 @@
+import csv
 import os
+import sys
 from csv import DictReader
 from operator import attrgetter
 from pathlib import Path
-
+import yaml
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -33,11 +35,24 @@ def load_coverage(csv):
             result[int(row['query_nuc_pos'])] = int(row['coverage'])
     return result
 
+BATCH = 'batch_01'
+
 ROOT = (
     Path('/wow')
+    / BATCH
     / 'Results'
     / 'scratch'
 )
+
+DATA = (
+    Path(os.path.realpath(__file__)).parent.parent
+    / 'data'
+)
+
+
+def load_yaml(yaml_file):
+    with open(yaml_file) as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
 # end Dan stuff
 
 # Source: https://github.com/PoonLab/sam2conseq/wiki
@@ -76,16 +91,16 @@ SRR11085741	RNA-Seq	2020-02-13	MN611517	non-human, failure to map
 SRR11085797	RNA-Seq	2020-02-13	MN996532.1
 SRR11314339	RNA-Seq	2020-03-17	MT192765
 SRR11092056	RNA-Seq	2020-02-15	MN996530	Low coverage, sample is mostly human DNA (known issue)	Y
-SRR11578349	RNA-Seq	2020-05-04	EPI_ISL_427024
-SRR11578341	RNA-Seq	2020-05-04	EPI_ISL_426901
-SRR11578342	RNA-Seq	2020-05-04	EPI_ISL_426900
-SRR11578343	RNA-Seq	2020-05-04	EPI_ISL_426899
-SRR11578344	RNA-Seq	2020-05-04	EPI_ISL_426899
-SRR11578345	RNA-Seq	2020-05-04	EPI_ISL_426656
-SRR11578346	RNA-Seq	2020-05-04	EPI_ISL_426898
-SRR11578347	RNA-Seq	2020-05-04	EPI_ISL_427026
-SRR11578348	RNA-Seq	2020-05-04	EPI_ISL_427025
-SRR11578349	RNA-Seq	2020-05-04	EPI_ISL_427024
+SRR11578349_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_427024
+SRR11578341_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426901
+SRR11578342_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426900
+SRR11578343_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426899
+SRR11578344_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426899
+SRR11578345_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426656
+SRR11578346_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_426898
+SRR11578347_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_427026
+SRR11578348_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_427025
+SRR11578349_1.fastq	RNA-Seq	2020-05-04	EPI_ISL_427024
 SRR11593354_1.fastq	RNA-Seq	2020-05-08	EPI_ISL_414507
 SRR11593355_1.fastq	RNA-Seq	2020-05-08	EPI_ISL_414574
 SRR11593356_1.fastq	RNA-Seq	2020-05-08	EPI_ISL_414509
@@ -111,7 +126,7 @@ def main():
     summaries = []
     source_path = Path(__file__)
     working_path = source_path.parent.parent.parent / 'micall' / 'tests' / 'working'
-    conseq_path = working_path / 'conseq_all.csv'
+    conseq_path = working_path / f'{BATCH}_conseq_all.csv'
     accessions_path = working_path / 'fetched_accessions.fasta'
     accessions_file = accessions_path.open('a')
     accessions_index = None
@@ -161,8 +176,15 @@ def main():
 
 
 def compare_using_gotoh(row, conseq_accession, published_seq):
+
+    # try:
+    #     nlocs = yaml.load(open(DATA / f'{conseq_accession}.yaml'), Loader=yaml.SafeLoader)
+    # except Exception as e:
+    #     print(e)
+    #     nlocs = []
     conseq = row['sequence'].replace('x', '')
     sample_name = row['sample']
+    n_counts = load_yaml(DATA / 'n_counts.yaml')
     header = f'{conseq_accession} => {sample_name}'
     print(header)
     print(len(conseq), len(published_seq))
@@ -173,12 +195,12 @@ def compare_using_gotoh(row, conseq_accession, published_seq):
     print(aln_conseq[:screen_width])
     print('...', aln_pub_seq[-screen_width+4:])
     print('...', aln_conseq[-screen_width+4:])
-    mismatch_count = add_count = missing_count = 0
+    mismatch_count = add_count = missing_count = matching_count = 0
 
     mycsv = ROOT / sample_name / 'genome_coverage.csv'
-    conseq = ROOT / sample_name / 'conseq_all.csv'
+    conseq_csv = ROOT / sample_name / 'conseq_all.csv'
     offset = None
-    with open(conseq) as csvfile:
+    with open(conseq_csv) as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             offset = int(row['seed-offset'])
@@ -187,20 +209,23 @@ def compare_using_gotoh(row, conseq_accession, published_seq):
     # matchfile = open(ROOT / sample_name / 'mymatches.txt', 'w')
     # mismatchfile = open(ROOT / sample_name / 'mymismatches.txt', 'w')
     # addedfile = open(ROOT / sample_name / 'myadded.txt', 'w')
-    datafile = open(ROOT / sample_name / 'data.tsv', 'w')
-    header = '\t'.join([
+    datafile = open(ROOT / sample_name / 'data.csv', 'w')
+    header = [
         'sample',
         'type',
         'pos',
         'micall_base',
         'gisaid_base',
         'ref_base',
-        'coverage',
+        # 'is_nloc',
+        'coverage'
         # 'mean_quality'
-    ])
-    datafile.write(header + '\n')
+    ]
+    writer = csv.DictWriter(datafile, header)
+    writer.writeheader()
     for i, (theirs, ours) in enumerate(zip(aln_pub_seq, aln_conseq)):
         pos = i + 1 + offset
+        # is_nloc = True if pos in nlocs else False
         coverage = '0'
         refbase = '?'
         try:
@@ -217,43 +242,49 @@ def compare_using_gotoh(row, conseq_accession, published_seq):
             #     matchfile.write(str(coverages[i]) + '\n')
             # except KeyError:
             #     matchfile.write('0' + '\n')
-            pass
+            matching_count += 1
         elif ours == '-':
-            datafile.write('\t'.join([
-                sample_name,
-                'deletion',
-                pos,
-                ours,
-                theirs,
-                refbase,
-                coverage
-            ]) + '\n')
+            my_type = 'deletion'
+            writer.writerow({
+                'sample': sample_name,
+                'type': my_type,
+                'pos': pos,
+                'micall_base': ours,
+                'gisaid_base': theirs,
+                'ref_base': refbase,
+                # 'is_nloc': is_nloc,
+                'coverage': coverage
+            })
             missing_count += 1
         elif theirs == '-':
-            datafile.write('\t'.join([
-                sample_name,
-                'addition',
-                pos,
-                ours,
-                theirs,
-                refbase,
-                coverage
-            ]) + '\n')
+            my_type = 'addition'
+            writer.writerow({
+                'sample': sample_name,
+                'type': my_type,
+                'pos': pos,
+                'micall_base': ours,
+                'gisaid_base': theirs,
+                'ref_base': refbase,
+                # 'is_nloc': is_nloc,
+                'coverage': coverage
+            })
             add_count += 1
             # try:
             #     addedfile.write(str(coverages[i]) + '\n')
             # except KeyError:
             #     addedfile.write('0' + '\n')
         else:
-            datafile.write('\t'.join([
-                sample_name,
-                'mismatch',
-                pos,
-                ours,
-                theirs,
-                refbase,
-                coverage
-            ]) + '\n')
+            my_type = 'mismatch'
+            writer.writerow({
+                'sample': sample_name,
+                'type': my_type,
+                'pos': pos,
+                'micall_base': ours,
+                'gisaid_base': theirs,
+                'ref_base': refbase,
+                # 'is_nloc': is_nloc,
+                'coverage': coverage
+            })
             # try:
             #     mismatchfile.write(str(coverages[i]) + '\n')
             # except KeyError:
@@ -263,11 +294,44 @@ def compare_using_gotoh(row, conseq_accession, published_seq):
                 print(f'{i}: {theirs} => {ours}')
     summary = (f'{mismatch_count} mismatches, {missing_count} missing, and '
                f'{add_count} added out of {len(published_seq)}.')
-    datafile.close()
     # matchfile.close()
     # mismatchfile.close()
     # addedfile.close()
     print(summary)
+
+    with open(ROOT / sample_name / 'data_extended.csv', 'w') as o:
+        header = [
+            'sample',
+            'conseq_len',
+            'ref_len',
+            'matches',
+            'mismatches',
+            'missing',
+            'added',
+            'num_n_removed',
+            'concordance'
+        ]
+        csv_writer = csv.DictWriter(o, header)
+        csv_writer.writeheader()
+
+        # Compute data
+        conseq_len = len(conseq)
+        ref_len = len(published_seq)
+        disagree = sum([mismatch_count, missing_count, add_count])
+        removed_n = n_counts[conseq_accession]
+        concordance = matching_count / ref_len * 100
+        csv_writer.writerow({
+            'sample': sample_name,
+            'conseq_len': conseq_len,
+            'ref_len': ref_len,
+            'matches': matching_count,
+            'mismatches': mismatch_count,
+            'missing': missing_count,
+            'added': add_count,
+            'num_n_removed': n_counts[conseq_accession],
+            'concordance': round(concordance, 2)
+        })
+
     return f'{sample_name}|{conseq_accession}|{summary}'
 
 
