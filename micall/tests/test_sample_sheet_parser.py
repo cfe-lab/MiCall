459ec49344ed444524757d4238eb1d1774666195
Donaim <15235025+Donaim@users.noreply.github.com>
2023-09-11 10:23:49 -0700

    Update micall/tests/test_sample_sheet_parser.py
    Co-authored-by: Don Kirkby <donkirkby@users.noreply.github.com>

--- micall/tests/test_sample_sheet_parser.py
+++ micall/tests/test_sample_sheet_parser.py
@@ -731,7 +731,6 @@ Chemistry:Sample2_Proj2:BreakingBad Disablecontamcheck:Sample2_Proj2:TRUE,
         self.assertEqual(ss["Experiment Name"], "10-Jul-2014")
 
 
-    # @unittest.expectedFailure
     def test_underscores_in_sample_name(self):
         """
         Extracts the correct project code having multiple options.
