d3c4aa7cb383bb24fe1d9e6b18e9b0927210758f
Vitaliy Mysak <vmysak@bccfe.ca>
2023-11-06 08:47:42 -0800

    Add tests for the new contig stitcher

--- micall/tests/test_contig_stitcher.py
+++ micall/tests/test_contig_stitcher.py
@@ -2,6 +2,44 @@ import pytest
 from micall.core.contig_stitcher import stitch_contigs, GenotypedContig
 
 
+def test_1():
+    contigs = [
+        GenotypedContig(name='a',
+                        seq='ACTGACTG' * 100,
+                        ref_name='testref',
+                        ref_seq='ACTGACTG' * 100,
+                        matched_fraction=1.0,
+                        ),
+        ]
+
+    result = list(stitch_contigs(contigs))
+    assert sorted(map(lambda x: x.seq, contigs)) \
+        == sorted(map(lambda x: x.seq, result))
+
+
+def test_2():
+    ref_seq = 'A' * 100
+
+    contigs = [
+        GenotypedContig(name='a',
+                        seq=ref_seq,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        GenotypedContig(name='b',
+                        seq='C' * 100,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        ]
+
+    result = list(stitch_contigs(contigs))
+    assert sorted(map(lambda x: x.seq, contigs)) \
+        == sorted(map(lambda x: x.seq, result))
+
+
 def test_3():
     ref_seq = 'A' * 100 + 'C' * 100
 
