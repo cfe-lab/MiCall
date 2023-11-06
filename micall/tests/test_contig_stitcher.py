00ee9fec8a75eb080f698f81a09b993449d7c4d6
Vitaliy Mysak <vmysak@bccfe.ca>
2023-11-06 15:35:31 -0800

    Improve basic config stitcher tests

--- micall/tests/test_contig_stitcher.py
+++ micall/tests/test_contig_stitcher.py
@@ -8,7 +8,9 @@ def mock_mappy_aligner(monkeypatch):
     monkeypatch.setattr('micall.core.contig_stitcher.Aligner', MockAligner)
 
 
-def test_1():
+def test_identical_stitching_of_one_contig():
+    # Scenario: When stitching one contig, it remains the same.
+
     contigs = [
         GenotypedContig(name='a',
                         seq='ACTGACTG' * 100,
@@ -23,7 +25,9 @@ def test_1():
         == sorted(map(lambda x: x.seq, result))
 
 
-def test_2():
+def test_separate_stitching_of_non_overlapping_contigs():
+    # Scenario: When stitching multiple non-overlapping contigs, the order doesn't matter.
+
     ref_seq = 'A' * 100
 
     contigs = [
@@ -45,8 +49,29 @@ def test_2():
     assert sorted(map(lambda x: x.seq, contigs)) \
         == sorted(map(lambda x: x.seq, result))
 
+    contigs = [
+        GenotypedContig(name='b',
+                        seq='C' * 100,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        GenotypedContig(name='a',
+                        seq=ref_seq,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        ]
 
-def test_3():
+    result = list(stitch_contigs(contigs))
+    assert sorted(map(lambda x: x.seq, contigs)) \
+        == sorted(map(lambda x: x.seq, result))
+
+
+def test_correct_stitching_of_two_partially_overlapping_contigs():
+    # Scenario: Two partially overlapping contigs are stitched correctly into a single sequence.
+
     ref_seq = 'A' * 100 + 'C' * 100
 
     contigs = [
@@ -74,7 +99,9 @@ def test_3():
     assert result.contig.name == 'a+overlap(a,b)+b'
 
 
-def test_4():
+def test_correct_processing_of_two_overlapping_and_one_separate_contig():
+    # Scenario: Two overlapping contigs are stitched together, the non-overlapping is kept separate.
+
     ref_seq = 'A' * 100 + 'C' * 100 + 'T' * 100
 
     contigs = [
@@ -106,3 +133,91 @@ def test_4():
     assert result[0].contig.name == 'a+overlap(a,b)+b'
 
     assert result[1].contig == contigs[2]
+
+
+def test_stitching_of_all_overlapping_contigs_into_one_sequence():
+    # Scenario: All contigs have some overlapping parts, resulting in one continuous sequence after stitching.
+
+    ref_seq = 'A' * 100 + 'C' * 100 + 'T' * 100
+
+    contigs = [
+        GenotypedContig(name='a',
+                        seq='A' * 50 + 'C' * 20,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        GenotypedContig(name='b',
+                        seq='A' * 20 + 'C' * 100 + 'T' * 20,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        GenotypedContig(name='c',
+                        seq='C' * 20 + 'T' * 50,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        ]
+
+    result = list(stitch_contigs(contigs))
+    assert len(result) == 1
+
+    result = result[0]
+
+    assert 200 == len(result.seq)
+    assert result.seq == 'A' * 50 + 'C' * 100 + 'T' * 50
+    assert result.contig.name == 'a+overlap(a,b)+b+overlap(a+overlap(a,b)+b,c)+c'
+
+
+def test_stitching_with_empty_contigs():
+    # Scenario: The function is able to handle and ignore empty contigs.
+
+    ref_seq = 'A' * 100
+
+    contigs = [
+        GenotypedContig(name='a',
+                        seq=ref_seq,
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        GenotypedContig(name='b',
+                        seq='',
+                        ref_name='testref',
+                        ref_seq=ref_seq,
+                        matched_fraction=0.5,
+                        ),
+        ]
+
+    result = list(stitch_contigs(contigs))
+    assert sorted(map(lambda x: x.seq, contigs)) \
+        == sorted(map(lambda x: x.seq, result))
+
+
+def test_stitching_of_identical_contigs():
+    # Scenario: The function correctly handles and avoids duplication when identical contigs are stitched together.
+
+    ref_seq = 'A' * 100
+    contig = \
+        GenotypedContig(name='a',
+                        seq='ACTGACTG' * 100,
+                        ref_name='testref',
+                        ref_seq='ACTGACTG' * 100,
+                        matched_fraction=1.0,
+                        )
+
+    contigs = [contig, contig, contig]
+
+    result = list(stitch_contigs(contigs))
+    assert len(result) == 1
+    assert result[0].contig == contig
+
+
+def test_stitching_of_zero_contigs():
+    # Scenario: The function does not crash if no contigs given.
+
+    contigs = []
+    result = list(stitch_contigs(contigs))
+    assert result == contigs
