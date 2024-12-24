9ee3136c436ca254670447475febc9e7ed43aa13
Vitaliy Mysak <vmysak@bccfe.ca>
2024-12-24 13:40:07 -0800

    Add tests for find_maximum_overlap

---
+++ micall/tests/test_merge_contigs.py
@@ -0,0 +1,20 @@
+import pytest
+import micall.utils.merge_contigs as merge_contigs
+
+
+@pytest.mark.parametrize(
+    "left, right, expected",
+    [
+        ('tttttxxxxx', 'uuuuuxxx', -10),
+        ('tttttxxx', 'uuuuuxxxxx', -10),
+        ('xxxttttt', 'xxxxxuuuuu', -10),
+        ('xxxxxttttt', 'xxxuuuuu', -10),
+        ('xxxxttttt', 'uuuuuxxx', -14),
+        ('aaaaxxxxttttt', 'uxxxk', -10),
+        ('tttttxxxx', 'xxxuuuuu', -3),
+        ('tttttxxxx', 'xxxuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', -3),
+        ('tttttxxxxpppppppppppppppppppp', 'xxxuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', -23),
+    ],
+)
+def test_maximum_overlap_cases(left, right, expected):
+    assert merge_contigs.find_maximum_overlap(left, right) == expected
