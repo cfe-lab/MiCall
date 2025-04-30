360640cd4a4ae07d2fad8d2a47ba435d6b7d1863
Vitaliy Mysak <vmysak@bccfe.ca>
2025-04-30 09:17:56 -0700

    fix handling of missing stats.json

--- micall/utils/analyze_kive_batches/combine_runs_overlaps.py
+++ micall/utils/analyze_kive_batches/combine_runs_overlaps.py
@@ -5,6 +5,7 @@ import csv
 
 from micall.utils.dir_path import DirPath
 from micall.utils.new_atomic_file import new_atomic_text_file
+from .logger import logger
 
 
 FIELDNAMES = ("app",
@@ -23,6 +24,10 @@ def combine_runs_overlaps(root: DirPath, runs_txt: Path, target: Path) -> None:
         dwriter.writeheader()
         for run_id in run_ids:
             stats = root / "runs" / str(run_id) / "stats.json"
+            if not stats.exists():
+                logger.debug("No stats file for run %s.", run_id)
+                continue
+
             with open(stats) as stats_reader:
                 stats_object = json.load(stats_reader)
 
