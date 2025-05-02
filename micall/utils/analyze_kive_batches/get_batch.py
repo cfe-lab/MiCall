590344a2ca2095cdd43214558d19cdda7ae19256
Vitaliy Mysak <vmysak@bccfe.ca>
2025-05-01 17:39:26 -0700

    Only call kivecli via its API

--- micall/utils/analyze_kive_batches/get_batch.py
+++ micall/utils/analyze_kive_batches/get_batch.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
-import subprocess
+import json
+import kivecli.findbatches
+import kivecli.login
 
 from micall.utils.user_error import UserError
 from .batch import BatchName
@@ -9,9 +11,8 @@ from .batch import BatchName
 def get_batch(batch: BatchName, target: Path) -> None:
     with target.open("wt") as writer:
         try:
-            subprocess.check_call(
-                ["kivecli", "findbatches", "--debug", "--json", "--name", str(batch)],
-                stdout=writer,
-            )
+            with kivecli.login.login():
+                batches = list(kivecli.findbatches.findbatches(name=str(batch)))
+                json.dump(batches, writer, indent='\t')
         except BaseException as ex:
             raise UserError("Work failed: %s", str(ex)) from ex
