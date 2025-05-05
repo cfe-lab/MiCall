ac61b2e8477e0765298e1b0f9866b6e7d38ce47f
Vitaliy Mysak <vmysak@bccfe.ca>
2025-05-05 14:46:30 -0700

    do not filter in extract_run_ids

--- micall/utils/analyze_kive_batches/extract_run_ids.py
+++ micall/utils/analyze_kive_batches/extract_run_ids.py
@@ -3,7 +3,6 @@ import json
 from pathlib import Path
 
 from micall.utils.new_atomic_file import new_atomic_text_file
-from .logger import logger
 
 
 def extract_run_ids(input: Path, output: Path) -> None:
@@ -12,9 +11,4 @@ def extract_run_ids(input: Path, output: Path) -> None:
         with new_atomic_text_file(output) as writer:
             for run in runs:
                 run_id = run["id"]
-                state = run["state"]
-                if state != "C":
-                    logger.warning("Skipping run %s: state '%s' != 'C'.", run_id, state)
-                    continue
-
                 writer.write(str(run_id) + "\n")
