19648bcb2374d427f3bff72bcfbfe046c07d2826
Vitaliy Mysak <vmysak@bccfe.ca>
2025-09-05 00:20:34 +0000

    Fix usage of exponentials

--- micall/utils/referenceless_contig_stitcher.py
+++ micall/utils/referenceless_contig_stitcher.py
@@ -79,7 +79,7 @@ def calculate_referenceless_overlap_score(L: int, M: int) -> Score:
     base = calculate_overlap_score(L=L, M=M) - ACCEPTABLE_BASE_STITCHING_SCORE()
     sign = 1 if base >= 0 else -1
     abase = sign * base # Absolute value of base.
-    magnitude = round((99 * abase + 2) ** 2) ^ 3
+    magnitude = round((99 * abase + 2) ** 2) ** 3
     return sign * magnitude
 
 
