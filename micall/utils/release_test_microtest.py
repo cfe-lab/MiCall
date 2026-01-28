ef03bc71a6fa3b37f93a800600e399b25f7192d5
Vitaliy Mysak <vmysak@bccfe.ca>
2026-01-28 22:32:35 +0000

    Add stitcher_plot_svg in microtest and update documentation for denovo assembly

--- micall/utils/release_test_microtest.py
+++ micall/utils/release_test_microtest.py
@@ -470,6 +470,7 @@ class SampleRunner:
                 'unstitched_conseq.csv',
                 'unstitched_contigs.csv',
                 'contigs.csv',
+                'stitcher_plot_svg',
                 'read_entropy.csv',
                 'conseq_region.csv',
                 'conseq_stitched.csv',
