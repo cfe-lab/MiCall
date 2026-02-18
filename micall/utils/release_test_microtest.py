4c9c7c67ac48567a9e2ef3c03687fb78051eb280
Vitaliy Mysak <vmysak@bccfe.ca>
2026-02-18 19:05:00 +0000

    Update remaining references to underscore executables

--- micall/utils/release_test_microtest.py
+++ micall/utils/release_test_microtest.py
@@ -587,10 +587,10 @@ class SampleRunner:
                        ]
 
             app_arguments = {
-                None: ['micall_kive'],
-                'filter_quality': ['filter_quality'],
-                'resistance': ['micall_kive_resistance'],
-                'denovo': ['micall_kive', '--denovo'],
+                None: ['micall-kive'],
+                'filter_quality': ['filter-quality'],
+                'resistance': ['micall-kive-resistance'],
+                'denovo': ['micall-kive', '--denovo'],
             }[app_name]
 
             command.extend(app_arguments)
