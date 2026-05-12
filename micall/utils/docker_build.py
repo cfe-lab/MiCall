9ae500b3237f8ed6a972f419d0a8fb5af948dde9
Vitaliy Mysak <vmysak@bccfe.ca>
2026-05-12 23:56:41 +0000

    Refactor build function to use context manager for source path and improve error handling

--- micall/utils/docker_build.py
+++ micall/utils/docker_build.py
@@ -24,54 +24,66 @@ def get_parser() -> ArgumentParser:
     return parser
 
 
-def build(source_path: str, repository_name: str) -> None:
-    with root_directory() as source_path:
-        try:
-            subprocess.check_call(['docker',
-                                   'build',
-                                   '--tag', repository_name,
-                                   '--', source_path])
-        except subprocess.CalledProcessError as ex:
-            if ex.returncode == errno.EPERM:
-                raise PermissionError(
-                    'Docker build failed. Do you have root permission?') from ex
-            raise
-
-
-def main(argv: Sequence[str]) -> None:
-    parser = get_parser()
-    args = parser.parse_args(argv)
+def get_repository_name() -> str:
+    """
+    Get the repository name for the docker image, which is based on the version from git describe.
+    """
 
     with root_directory() as source_path:
         version = subprocess.check_output(['git',
                                             '-c', 'core.fileMode=false',  # Ignore file mode
                                             '-c', 'safe.directory=*',  # Allow git commands in any directory
                                             'describe',
-                                            '--tags',
-                                            '--dirty'],
+                                            '--tags'],
                                             cwd=source_path,
                                             text=True).strip()
 
     if version.startswith('v'):
         version = version[1:]
 
-    repository_name = 'docker.illumina.com/cfe_lab/micall'
-    if args.tag is not None:
-        repository_name += ':' + version
+    return f'docker.illumina.com/cfe_lab/micall:{version}'
+
+
+def build() -> str:
+    """
+    Build the docker image from the local source code.
+    The image will be tagged with the version from git describe, and pushed to docker.illumina.com/cfe_lab/micall.
+    If the version is not tagged, it will be tagged with the latest tag plus a suffix indicating the number of commits since the latest tag and the short sha of the current commit.
+    If there are uncommitted changes, the version will have a -dirty suffix.
+    Returns the repository name of the built image.
+    """
+
+    repository_name = get_repository_name()
 
     try:
-        subprocess.check_call(['docker',
-                               'build',
-                               '--tag', repository_name,
-                               '--', source_path])
+        with root_directory() as source_path:
+            try:
+                subprocess.check_call(['docker',
+                                    'build',
+                                    '--tag', repository_name,
+                                    '--', str(source_path)])
+            except subprocess.CalledProcessError as ex:
+                if ex.returncode == errno.EPERM:
+                    raise PermissionError(
+                        'Docker build failed. Do you have root permission?') from ex
+                raise
     except subprocess.CalledProcessError as ex:
         if ex.returncode == errno.EPERM:
             raise PermissionError(
                 'Docker build failed. Do you have root permission?') from ex
         raise
+
+    return repository_name
+
+
+def main(argv: Sequence[str]) -> None:
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    build()
     if args.nopush:
         print("Rerun without --nopush to attempt to push the docker image to illumina and launch spacedock")
         exit()
+    repository_name = get_repository_name()
     subprocess.check_call(['docker', 'push', '--', repository_name])
     if args.agent_id is None:
         print('Docker image pushed. Include an agent id to launch spacedock.')
